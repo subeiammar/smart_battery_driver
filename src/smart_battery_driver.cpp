fa2d2cb322ef186ae2f13c463d6590d55d56302c
almond <ammar@bloomfield.ai>
2020-03-10 16:48:36 -0400

    Implements string output for battery status; adds gitignore

--- src/smart_battery_driver.cpp
+++ src/smart_battery_driver.cpp
@@ -14,6 +14,7 @@
 
 #include <memory>
 #include <string>
+#include <iostream>
 #include <algorithm>
 
 #include "sbs.hpp"
@@ -26,100 +27,108 @@ class SmartBatteryDriver
     {
       setvbuf(stdout, NULL, _IONBF, BUFSIZ);
 
-      // TODO
+      // TODO:
+      // get device path using `i2cdetect -l`
+      // get device address using `i2cdetect -y -r <bus_num>`
       const std::string device_path = "";
       const int battery_address = 666;
 
       battery_ = std::make_unique<SBS::SmartBattery>(device_path.c_str(), battery_address);
     }
 
-  private:
-    uint8_t interpret_status(int status, float current) const
+    void publish_battery_state()
     {
-      using SBS::BatteryStatus;
-      if (status & static_cast<int>(BatteryStatus::Discharging)) {
-        //return BatteryState::POWER_SUPPLY_STATUS_DISCHARGING;
-      }
-      // TODO(ek) figure out what this condition means
-      // return POWER_SUPPLY_STATUS_NOT_CHARGING;
-      if (status & static_cast<int>(BatteryStatus::FullyCharged)) {
-        //return BatteryState::POWER_SUPPLY_STATUS_FULL;
-      }
-      if (current > 0) {
-        //return BatteryState::POWER_SUPPLY_STATUS_CHARGING;
-      }
+      //msg_ = std::make_unique<sensor_msgs::msg::BatteryState>();
+      //msg_->header.stamp = get_clock()->now();
+      //const int charge = -msg_->current;
+      
+      const int voltage = battery_->voltage() / 1000; 
+      const int capacity = battery_->fullChargeCapacity() / 1000;
+      const int design_capacity = battery_->designCapacity() / 1000;
+      const int percentage = battery_->relativeStateOfCharge() / 100;
 
-      //return BatteryState::POWER_SUPPLY_STATUS_UNKNOWN;
+      const float current = battery_->current() / 1000.0;
+      const float temperature = battery_->temperature();
+
+      int status = battery_->batteryStatus();
+
+      const std::string serial_number = std::to_string(battery_->serialNumber());
+
+      std::string chemistry;
+      battery_->cellChemistry(chemistry);
+      const std::string power_supply_technology = interpret_chemistry(chemistry);
+      const std::string power_supply_status = interpret_status(status, current);
+      const std::string power_supply_health = interpret_health(status);
+
+      // TODO(ek) detect case when it's not present
+      //msg_->present = true;
+      // msg->cell_voltage =
+      // msg->location =
+      
+      std::cout << std::endl;
+      std::cout << "Voltage: " << voltage << "V "
+        << "| Amperage: " << current << "A "
+        << "| Percentage: " << percentage << "%" << std::endl;
+      std::cout << "PS Status: " << power_supply_status << std::endl;
+      std::cout << "PS Health: " << power_supply_health << std::endl;
+      std::cout << std::endl;
     }
 
-    uint8_t interpret_health(int status) const
+  private:
+    std::string interpret_status(int status, float current) const
+    {
+      using SBS::BatteryStatus;
+      std::string output = "Unknown";
+
+      if (status & static_cast<int>(BatteryStatus::Discharging)) {
+        output = "Discharging";
+      }
+      if (status & static_cast<int>(BatteryStatus::FullyCharged)) {
+        output = "Full";
+      }
+      if (current > 0) {
+        output += "|Charging";
+      }
+
+      // TODO(ek) figure out what this condition means
+      // return POWER_SUPPLY_STATUS_NOT_CHARGING;
+
+      return output;
+    }
+
+    std::string interpret_health(int status) const
     {
       using SBS::BatteryStatusAlarm;
       using SBS::BatteryStatus;
+      std::string output = "Good";
 
       if (status & static_cast<int>(BatteryStatusAlarm::OverTemp)) {
-        //return BatteryState::POWER_SUPPLY_HEALTH_OVERHEAT;
+        output = "Overheat";
       }
       if (status & static_cast<int>(BatteryStatusAlarm::OverCharged)) {
-        //return BatteryState::POWER_SUPPLY_HEALTH_OVERVOLTAGE;
+        output = "Overcharged";
       }
       if (status & static_cast<int>(BatteryStatus::FullyDischarged)) {
-        //return BatteryState::POWER_SUPPLY_HEALTH_DEAD;
+        output = "Dead/FullyDischarged";
       }
       if (status & static_cast<int>(BatteryStatusAlarm::Mask)) {
         // Any alarm set and haven't returned yet
-        //return BatteryState::POWER_SUPPLY_HEALTH_UNSPEC_FAILURE;
+        output = "Unspec?";
       }
 
-      //return BatteryState::POWER_SUPPLY_HEALTH_GOOD;
+      return output;
     }
 
-    uint8_t interpret_chemistry(const std::string & chemistry)
+    std::string interpret_chemistry(const std::string & chemistry)
     {
-      std::string lower(chemistry);
-      std::transform(lower.begin(), lower.end(), lower.begin(),
+      std::string output(chemistry);
+      std::transform(output.begin(), output.end(), output.begin(),
           [] (unsigned char c)
           {
             return std::tolower(c);
           });
 
-      if (lower == "nimh") {
-        //return BatteryState::POWER_SUPPLY_TECHNOLOGY_NIMH;
-      } else if (lower == "lion") {
-        //return BatteryState::POWER_SUPPLY_TECHNOLOGY_LION;
-      } else if (lower == "lip") {
-        //return BatteryState::POWER_SUPPLY_TECHNOLOGY_LIPO;
-      } else if (lower == "nicd") {
-        //return BatteryState::POWER_SUPPLY_TECHNOLOGY_NICD;
-      } else {
-        //return BatteryState::POWER_SUPPLY_TECHNOLOGY_UNKNOWN;
-      }
-    }
-
-    void publish_battery_state()
-    {
-      //msg_ = std::make_unique<sensor_msgs::msg::BatteryState>();
-      //msg_->header.stamp = get_clock()->now();
-      //msg_->voltage = battery_->voltage() / 1000.0;
-      //msg_->temperature = battery_->temperature();
-      //msg_->current = battery_->current() / 1000.0;
-      //msg_->charge = -msg_->current;
-      //msg_->capacity = battery_->fullChargeCapacity() / 1000.0;
-      //msg_->design_capacity = battery_->designCapacity() / 1000.0;
-      //msg_->percentage = battery_->relativeStateOfCharge() / 100.0;
-
-      int status = battery_->batteryStatus();
-      //msg_->power_supply_status = interpret_status(status, msg_->current);
-      //msg_->power_supply_health = interpret_health(status);
-
-      std::string chemistry;
-      battery_->cellChemistry(chemistry);
-      //msg_->power_supply_technology = interpret_chemistry(chemistry);
-      // TODO(ek) detect case when it's not
-      //msg_->present = true;
-      // msg->cell_voltage =
-      // msg->location =
-      //msg_->serial_number = std::to_string(battery_->serialNumber());
+      return output;
     }
 
   private:
@@ -131,8 +140,7 @@ int main(int argc, char ** argv)
   setvbuf(stdout, NULL, _IONBF, BUFSIZ);
   auto driver = std::make_shared<SmartBatteryDriver>();
 
-  // TODO: get correct bus number then device address
-  // use address to query for device (RRC battery) state
+  driver->publish_battery_state();
 
   return 0;
 }
